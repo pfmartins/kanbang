d0b24dd8efd77acc3acf30f564eb9b5fa8cf1f46
diegohfcelestino <diegohfcelestino@gmail.com>
2022-04-04 11:59:45 -0300

    Configurando e rodando o projeto, instalado webpack, webpack-cli, loader do css e sass e o node-sass

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
+import React from "react";
+import "./styles/global.scss";
 const App = () => {
-    return (
-        <div>Hello World</div>
-    );
-}
+  return <div>Hello World</div>;
+};
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,45 @@
-'use strict';
+"use strict";
 
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.join(__dirname, '/dist'),
-        publicPath: '/'
-    },
-    devServer: {
-        allowedHosts: "all",
-        historyApiFallback: true
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /nodeModules/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            }
-        ]
-    },
-    plugins: [new HtmlWebpackPlugin({ inject: true, template: 'public/index.html' })],
-}
+  //devtool: "eval-source-map", //para renderizar o codigo exatamente como é (sem utilizar)
+  entry: path.resolve(__dirname, "src", "index.js"),
+  output: {
+    path: path.join(__dirname, "dist"),
+    publicPath: "/",
+    filename: "kanban.js",
+  },
+
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  devServer: {
+    allowedHosts: "all",
+    historyApiFallback: true,
+    port: 3030,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.(css|scss)$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: path.resolve(__dirname, "public", "index.html"),
+    }),
+  ],
+};
