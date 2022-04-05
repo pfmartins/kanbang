5685463193a31809a84211827e8b79f107a4b460
diegohfcelestino <diegohfcelestino@gmail.com>
2022-04-05 16:59:32 -0300

    Adicioando logout

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,20 @@
 import React from "react";
 
 export const Home = (props) => {
-  return <h1>Gerenciador de Tarefas</h1>;
+  return (
+    <>
+      <h1>Gerenciador de Tarefas</h1>
+      <a
+        onClick={(e) => {
+          localStorage.removeItem("");
+          localStorage.removeItem("accessToken");
+          localStorage.removeItem("usuarioNome");
+          localStorage.removeItem("usuarioEmail");
+          props.setAccessToken("");
+        }}
+      >
+        Sair
+      </a>
+    </>
+  );
 };
